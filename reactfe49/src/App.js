6540f9c9c9453ffb27c24aa75c87fe8c4135642a
unknown <vomanhdat10998@gmail.com>
2020-08-23 08:46:08 +0700

    'b2'

--- reactfe49/src/App.js
+++ reactfe49/src/App.js
@@ -5,6 +5,7 @@ import BaiTapLayout1 from "./Components/BaiTapLayout1/BaiTapLayout1";
 import BaiTapLayout2 from "./Components/BaiTapLayout2/BaiTapLayout2";
 import DataBinding from "./DataBinding/DataBinding";
 import HandleEvent from "./HandleEvent/HandleEvent";
+
 function App() {
   return (
     <div className="App">
