49f57b218607ece7f0cf6df349291869216b9c33
unknown <vomanhdat10998@gmail.com>
2020-09-16 18:47:32 +0700

    'master-1'

--- reactfe49/src/App.js
+++ reactfe49/src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
 import "./App.css";
-import BTGH from "./BaiTapRedux/BaiTapGioHangRedux/BTGH";
-import ModalRedux from "./BaiTapRedux/BaiTapGioHangRedux/ModalRedux";
 
 function App() {
-  return (
-    <div className="App">
-      <BTGH />
-      <ModalRedux />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
